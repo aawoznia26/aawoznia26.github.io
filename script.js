6cd58f5b0bca85c262c9ed99e24f370d258322ea
aawoznia2644_do_github <ania.woznia@gmail.com_do_github>
2019-07-22 23:22:43 +0200

    new controllers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://secure-harbor-92622.herokuapp.com/v1/task/';
+  const apiRoot = 'https://secure-harbor-92622.herokuapp.com/v1/';
   const trelloApiRoot = 'https://secure-harbor-92622.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
@@ -12,7 +12,7 @@ $(document).ready(function() {
   getAllTasks();
 
   function getAllAvailableBoards(callback, callbackArgs) {
-    var requestUrl = trelloApiRoot + 'getTrelloBoards';
+    var requestUrl = trelloApiRoot + 'boards';
 
     $.ajax({
       url: requestUrl,
@@ -38,9 +38,9 @@ $(document).ready(function() {
   function prepareBoardOrListSelectOptions(availableChoices) {
     return availableChoices.map(function(choice) {
       return $('<option>')
-                .addClass('crud-select__option')
-                .val(choice.id)
-                .text(choice.name || 'Unknown name');
+          .addClass('crud-select__option')
+          .val(choice.id)
+          .text(choice.name || 'Unknown name');
     });
   }
 
@@ -55,15 +55,15 @@ $(document).ready(function() {
       var $availableBoardsOptionElements = prepareBoardOrListSelectOptions(boards);
 
       $datatableRowEl.find('[data-board-name-select]')
-        .append($availableBoardsOptionElements);
+          .append($availableBoardsOptionElements);
 
       $datatableRowEl
-        .appendTo($tasksContainer);
+          .appendTo($tasksContainer);
     });
   }
 
   function getAllTasks() {
-    const requestUrl = apiRoot + 'getTasks';
+    const requestUrl = apiRoot + 'tasks';
 
     $.ajax({
       url: requestUrl,
@@ -84,7 +84,7 @@ $(document).ready(function() {
     var taskId = parentEl.attr('data-task-id');
     var taskTitle = parentEl.find('[data-task-name-input]').val();
     var taskContent = parentEl.find('[data-task-content-input]').val();
-    var requestUrl = apiRoot + 'updateTask';
+    var requestUrl = apiRoot + 'tasks';
 
     $.ajax({
       url: requestUrl,
@@ -108,12 +108,10 @@ $(document).ready(function() {
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parents('[data-task-id]');
     var taskId = parentEl.attr('data-task-id');
-    var requestUrl = apiRoot + 'deleteTask';
+    var requestUrl = apiRoot + 'tasks';
 
     $.ajax({
-      url: requestUrl + '/?' + $.param({
-        taskId: taskId
-      }),
+      url: requestUrl + '/' + taskId,
       method: 'DELETE',
       success: function() {
         parentEl.slideUp(400, function() { parentEl.remove(); });
@@ -127,7 +125,7 @@ $(document).ready(function() {
     var taskTitle = $(this).find('[name="title"]').val();
     var taskContent = $(this).find('[name="content"]').val();
 
-    var requestUrl = apiRoot + 'createTask';
+    var requestUrl = apiRoot + 'tasks';
 
     $.ajax({
       url: requestUrl,
@@ -168,7 +166,7 @@ $(document).ready(function() {
   }
 
   function handleCardCreationRequest(event) {
-    var requestUrl = trelloApiRoot + 'createTrelloCard';
+    var requestUrl = trelloApiRoot + 'card';
     var $relatedTaskRow = $(event.target).parents('[data-task-id]');
     var relatedTaskId = $relatedTaskRow.attr('data-task-id');
     var relatedTask = availableTasks[relatedTaskId];
